aa7abac1c2bebb33603fad464b26e7abe7bf697d
Laura Messa Travin <laura.travin@gmail.com>
2018-12-21 17:02:50 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,9 +8,12 @@ class Application
     if req.path.match(/items/)
 
       item_name = req.path.split("/items/").last     #turn /items/item_name into Sorry
-      item = @@items.find{|s| s.name == item_name }
-
-      resp.write item.price
+      if item = @@items.find{|s| s.name == item_name }
+          resp.write item.price
+      else
+          resp.write "Product not found"
+          resp.status = 400
+      end
     else
       resp.write "Route not found"
       resp.status = 404
